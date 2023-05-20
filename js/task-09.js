5fb1c6d38f1298979b4ab9662eb86bdc968faa0b
MarinaSigida <sigida.web@gmail.com>
2023-05-20 19:27:05 +0200

    update44444

--- js/task-09.js
+++ js/task-09.js
@@ -3,8 +3,6 @@
 // по кліку на button.change-color і виводить значення кольору в span.color.
 // Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
-
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
